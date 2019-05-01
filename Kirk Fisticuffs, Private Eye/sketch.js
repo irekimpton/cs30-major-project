a2fa5074c430f04435df0b56abfb6a70c1c61f23
irekimpton <47368861+irekimpton@users.noreply.github.com>
2019-05-01 15:20:58 -0600

    finished menu movement

--- Kirk Fisticuffs, Private Eye/sketch.js
+++ Kirk Fisticuffs, Private Eye/sketch.js
@@ -12,19 +12,22 @@ let inventorySize;
 let inventoryGridSize;
 let inventoryGrid;
 let inventoryScalar;
-let toolBar;
+let toolBar = [0, 0, 0, 0];;
 let inventoryHolding;
 
 let itemFrame;
 let knuckles;
 let potato;
 let squash;
+let imageList = [];
 
 function preload(){
   itemFrame = loadImage("assets/miscSprites/itemFrame.png");
   knuckles = loadImage("assets/items/knuckles.png");
   potato = loadImage("assets/items/potato.png");
   squash = loadImage("assets/items/squash.png");
+
+  imageList = [knuckles, potato, squash];
 }
 
 function setup() {
@@ -38,15 +41,14 @@ function setup() {
   rectMode(CENTER)
 
   inventoryGrid = create2dArray(inventoryGridSize, inventoryGridSize);
-  toolBar = [0, 0, 0, 0];
 
-  inventoryGrid[0][0] = "knuckles";
-  inventoryGrid[1][0] = "potato";
-  inventoryGrid[0][1] = "squash";
-  inventoryHolding = "0";
+  inventoryGrid[0][0] = 1;
+  inventoryGrid[1][0] = 2;
+  inventoryGrid[0][1] = 3;
+  inventoryHolding = 0;
 }
 
-///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////DrawLoops
 
 function draw() {
   if (gameMode === "menu"){
@@ -62,6 +64,24 @@ function draw() {
     }
     gameConstantDrawLoop();
   }
+}
+
+function gameDrawLoop(){
+  rect(mouseX, mouseY, height/12, height/12);
+}
+
+function pausedDrawLoop(){
+  rect(width/2, (height/2-height/24), height*3/4, height*3/4);
+  displayinventoryGrid();
+  if (inventoryHolding !== 0){
+    imageMode(CENTER);
+    image(imageList[inventoryHolding-1], mouseX, mouseY, inventoryScalar, inventoryScalar);
+  }
+}
+
+function gameConstantDrawLoop(){
+  rectMode(CENTER);
+  rect(width/2, height - height/24, height/3, height/12);
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -87,22 +107,6 @@ function mouseClicked(){
 
 ///////////////////////////////////////////////////////////////////////
 
-function gameDrawLoop(){
-  rect(mouseX, mouseY, height/12, height/12);
-}
-
-function pausedDrawLoop(){
-  rect(width/2, (height/2-height/24), height*3/4, height*3/4);
-  displayinventoryGrid();
-}
-
-function gameConstantDrawLoop(){
-  rectMode(CENTER);
-  rect(width/2, height - height/24, height*5/12, height/12);
-}
-
-
-
 function create2dArray(rows, columns){
   let emptyArray = [];
   for (i = 0; i < rows; i ++){
@@ -122,16 +126,9 @@ function displayinventoryGrid(){
       imageMode(CORNER);
       image(itemFrame, x*inventoryScalar, y*inventoryScalar, inventoryScalar, inventoryScalar);
       imageMode(CENTER);
-      if (inventoryGrid[x][y] === "knuckles"){
-        image(knuckles, x*inventoryScalar+inventoryScalar/2, y*inventoryScalar+inventoryScalar/2, inventoryScalar*4/5, inventoryScalar*4/5);
+      if (inventoryGrid[x][y] !== 0){
+        image(imageList[inventoryGrid[x][y]-1], x*inventoryScalar+inventoryScalar/2, y*inventoryScalar+inventoryScalar/2, inventoryScalar*4/5, inventoryScalar*4/5);
       }
-      if (inventoryGrid[x][y] === "potato"){
-        image(potato, x*inventoryScalar+inventoryScalar/2, y*inventoryScalar+inventoryScalar/2, inventoryScalar*4/5, inventoryScalar*4/5);
-      }
-      if (inventoryGrid[x][y] === "squash"){
-        image(squash, x*inventoryScalar+inventoryScalar/2, y*inventoryScalar+inventoryScalar/2, inventoryScalar*4/5, inventoryScalar*4/5);
-      }
-
     }
   }
   pop();
@@ -143,8 +140,8 @@ function pickUp(){
   translate(width/2 - height*3/8, (height/2-height/24) - height*3/8);
   let newMouseX = mouseX - width/2 - height*3/8;
   let newMouseY = mouseY - (height/2-height/24) - height*3/8;
-  let gridSpotX = floor(newMouseX/inventoryScalar + 4);
-  let gridSpotY = floor(newMouseY/inventoryScalar + 4);
+  let gridSpotX = floor(newMouseX/inventoryScalar + inventoryGridSize);
+  let gridSpotY = floor(newMouseY/inventoryScalar + inventoryGridSize);
   console.log(gridSpotX, gridSpotY);
 
   tempHold = inventoryGrid[gridSpotX][gridSpotY];
